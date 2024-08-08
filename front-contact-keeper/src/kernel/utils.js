54ec3c33954aab2f0b4de6a922500829c4c0b126
MeridaNoe <20213tn121@utez.edu.mx>
2024-08-08 01:06:21 -0600

    [ADD] consumo del login y id_token guardado en localstorage

--- front-contact-keeper/src/kernel/utils.js
+++ front-contact-keeper/src/kernel/utils.js
@@ -1,7 +1,39 @@
-export const getUserByName = (name, people) => {
-    return people.some(person => person.name === name);
-}
+UTILS:
 
-export const getUserByEmail = (email, people) => {
-    return people.some(person => person.email === email);
-}
+const getUserByName = (name, people) => {
+  return people.some((person) => person.name === name);
+};
+
+const getUserByEmail = (email, people) => {
+  return people.some((person) => person.email === email);
+};
+
+const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+const removeToken = () => {
+  localStorage.removeItem("token");
+};
+const getRoleNameByToken = async () => {
+  try {
+      const role = localStorage.getItem('role');
+      if (!role) {
+          throw new Error('No role found');
+      }
+
+      return role
+  } catch (error) {
+      removeToken();
+  }
+};
+
+
+
+export default {
+  getToken,
+  removeToken,
+  getRoleNameByToken,
+  getUserByEmail,
+  getUserByName,
+};
--- front-contact-keeper/src/modules/login/services/Access.js
+++ front-contact-keeper/src/modules/login/services/Access.js
@@ -1,13 +1,14 @@
 import api from '@/config/http-client.gateway'
 
 const login = async (credentials) => {
-    try {
-        return await api.doPost('/login', credentials)
-    }catch(error){
-        return error.response
-    }
-}
+  try {
+    const response = await api.doPost("https://zy2too7q94.execute-api.us-east-1.amazonaws.com/Prod/login", credentials);
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
 
 export default {
-    login
+  login
 }
