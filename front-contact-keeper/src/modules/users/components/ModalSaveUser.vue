e5d33471ea06042d25af4563eaae746cdd45d25c
MeridaNoe <20213tn121@utez.edu.mx>
2024-08-12 13:12:26 -0600

    [ADD] se agrego el GET de usuario en vista de admin

--- front-contact-keeper/src/modules/users/components/ModalSaveUser.vue
+++ front-contact-keeper/src/modules/users/components/ModalSaveUser.vue
@@ -1,13 +1,6 @@
 <template>
-  <Dialog
-      header="Crear nuevo usuario"
-      :modal="true"
-      :closeOnEscape="false"
-      :closable="false"
-      position="center"
-      :contentStyle="{overflow: 'visible', width: '50vw'}"
-      :visible.sync="visible"
-  >
+  <Dialog header="Crear nuevo usuario" :modal="true" :closeOnEscape="false" :closable="false" position="center"
+    :contentStyle="{ overflow: 'visible', width: '50vw' }" :visible.sync="visible">
     <div class="p-fluid grid">
       <b-row>
         <b-col cols="12">
@@ -17,16 +10,14 @@
           <div class="field">
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-user" />
-              <InputText id="field-name" v-model="v$.name.$model"
-                         :class="{ 'invalid-field-custom': v$.name.$error }"/>
+              <InputText id="field-name" v-model="v$.name.$model" :class="{ 'invalid-field-custom': v$.name.$error }" />
               <label for="field-name" class="form-label-required">Nombre</label>
             </span>
             <div class="text-danger text-start pt-2">
               <p class="error-messages" v-if="v$.name.$dirty && v$.name.required.$invalid">
                 {{ v$.name.required.$message }}
               </p>
-              <p class="error-messages"
-                 v-if="v$.name.$dirty && v$.name.onlyLetters.$invalid">
+              <p class="error-messages" v-if="v$.name.$dirty && v$.name.onlyLetters.$invalid">
                 {{ v$.name.onlyLetters.$message }}
               </p>
               <p class="error-messages" v-if="v$.name.$dirty && v$.name.minLength.$invalid">
@@ -43,15 +34,14 @@
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-user" />
               <InputText id="field-surname" v-model="v$.surname.$model"
-                         :class="{ 'invalid-field-custom': v$.surname.$error }"/>
+                :class="{ 'invalid-field-custom': v$.surname.$error }" />
               <label for="field-surname" class="form-label-required">Apellido paterno</label>
             </span>
             <div class="text-danger text-start pt-2">
               <p class="error-messages" v-if="v$.surname.$dirty && v$.surname.required.$invalid">
                 {{ v$.surname.required.$message }}
               </p>
-              <p class="error-messages"
-                 v-if="v$.surname.$dirty && v$.surname.onlyLetters.$invalid">
+              <p class="error-messages" v-if="v$.surname.$dirty && v$.surname.onlyLetters.$invalid">
                 {{ v$.surname.onlyLetters.$message }}
               </p>
               <p class="error-messages" v-if="v$.surname.$dirty && v$.surname.minLength.$invalid">
@@ -68,12 +58,11 @@
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-user" />
               <InputText id="field-lastname" v-model="v$.lastname.$model"
-                         :class="{ 'invalid-field-custom': v$.lastname.$error }"/>
+                :class="{ 'invalid-field-custom': v$.lastname.$error }" />
               <label for="field-lastname">Apellido materno</label>
             </span>
             <div class="text-danger text-start pt-2">
-              <p class="error-messages"
-                 v-if="v$.lastname.$dirty && v$.lastname.onlyLetters.$invalid">
+              <p class="error-messages" v-if="v$.lastname.$dirty && v$.lastname.onlyLetters.$invalid">
                 {{ v$.lastname.onlyLetters.$message }}
               </p>
             </div>
@@ -89,7 +78,7 @@
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-envelope" />
               <InputText id="field-email" v-model="v$.email.$model"
-                         :class="{ 'invalid-field-custom': v$.email.$error }"/>
+                :class="{ 'invalid-field-custom': v$.email.$error }" />
               <label for="field-email" class="form-label-required">Correo electrónico</label>
             </span>
             <div class="text-danger text-start pt-2">
@@ -107,15 +96,14 @@
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-phone" />
               <InputText id="field-phone" v-model="v$.phone.$model"
-                         :class="{ 'invalid-field-custom': v$.phone.$error }"/>
+                :class="{ 'invalid-field-custom': v$.phone.$error }" />
               <label for="field-phone" class="form-label-required">Teléfono</label>
             </span>
             <div class="text-danger text-start pt-2">
               <p class="error-messages" v-if="v$.phone.$dirty && v$.phone.required.$invalid">
                 {{ v$.phone.required.$message }}
               </p>
-              <p class="error-messages"
-                 v-if="v$.phone.$dirty && v$.phone.validPhone.$invalid">
+              <p class="error-messages" v-if="v$.phone.$dirty && v$.phone.validPhone.$invalid">
                 {{ v$.phone.validPhone.$message }}
               </p>
               <p class="error-messages" v-if="v$.phone.$dirty && v$.phone.minLength.$invalid">
@@ -130,13 +118,13 @@
       </b-row>
       <b-row>
         <b-col cols="12">
-          <h2 class="form-part" >Acceso</h2>
+          <h2 class="form-part">Acceso</h2>
         </b-col>
         <b-col class="mt-3 mb-2" cols="12" lg="12">
           <div class="field">
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-at" />
-              <Dropdown v-model="v$.user_type.$model" :options="userTypes" optionLabel="name"  optionValue="code"/>
+              <Dropdown v-model="v$.user_type.$model" :options="userTypes" optionLabel="name" optionValue="code" />
               <label for="field-lastname" class="form-label-required">Tipo:</label>
             </span>
           </div>
@@ -145,15 +133,15 @@
           <div class="field">
             <span class="p-float-label p-input-icon-right">
               <i class="pi pi-at" />
-              <InputText id="field-username" type="text" v-model="v$.username.$model" :class="{ 'invalid-field-custom': v$.username.$error }"/>
+              <InputText id="field-username" type="text" v-model="v$.username.$model"
+                :class="{ 'invalid-field-custom': v$.username.$error }" />
               <label for="field-username" class="form-label-required">Usuario</label>
             </span>
             <div class="text-danger text-start pt-2">
               <p class="error-messages" v-if="v$.username.$dirty && v$.username.required.$invalid">
                 {{ v$.username.required.$message }}
               </p>
-              <p class="error-messages"
-                 v-if="v$.username.$dirty && v$.username.onlyLetters.$invalid">
+              <p class="error-messages" v-if="v$.username.$dirty && v$.username.onlyLetters.$invalid">
                 {{ v$.username.onlyLetters.$message }}
               </p>
               <p class="error-messages" v-if="v$.username.$dirty && v$.username.minLength.$invalid">
@@ -168,7 +156,9 @@
         <b-col class="mt-3 mb-2" lg="6">
           <div class="field">
             <span class="p-float-label p-input-icon-right">
-              <Password v-model="v$.password.$model" :class="{ 'invalid-field-custom': v$.password.$error }"  toggleMask promptLabel="Elige una contraseña segura" weakLabel="Débil" mediumLabel="Medianamente segura" strongLabel="Muy segura" >
+              <Password v-model="v$.password.$model" :class="{ 'invalid-field-custom': v$.password.$error }" toggleMask
+                promptLabel="Elige una contraseña segura" weakLabel="Débil" mediumLabel="Medianamente segura"
+                strongLabel="Muy segura">
                 <template #header>
                   <h6>Escribe una contraseña</h6>
                 </template>
@@ -176,10 +166,10 @@
                   <Divider />
                   <p class="mt-2">Sugerencias</p>
                   <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
-                      <li>Al menos una minúscula</li>
-                      <li>Al menos una mayúscula</li>
-                      <li>Al menos un número</li>
-                      <li>Mínimo 8 caracteres</li>
+                    <li>Al menos una minúscula</li>
+                    <li>Al menos una mayúscula</li>
+                    <li>Al menos un número</li>
+                    <li>Mínimo 8 caracteres</li>
                   </ul>
                 </template>
               </Password>
@@ -195,34 +185,37 @@
       </b-row>
     </div>
     <template #footer>
-      <Button @click="saveUser()" label="Guardar" icon="pi pi-check"  iconPos="right" class="button-options"/>
-      <Button  label="Cancelar" icon="pi pi-times" class="p-button-text p-button-text p-button-plain" iconPos="right" @click="closeModal()"/>
+      <Button @click="saveUser()" label="Guardar" icon="pi pi-check" iconPos="right" class="button-options" />
+      <Button label="Cancelar" icon="pi pi-times" class="p-button-text p-button-text p-button-plain" iconPos="right"
+        @click="closeModal()" />
     </template>
   </Dialog>
 </template>
 
 <script>
-import {reactive} from "@vue/composition-api";
+import { reactive } from "@vue/composition-api";
 import useVuelidate from "@vuelidate/core";
 import { required, helpers, maxLength, minLength, email } from '@vuelidate/validators'
-import {nameRegex, noRequiredField, phoneRegex} from "@/kernel/patterns.js";
+import { nameRegex, noRequiredField, phoneRegex } from "@/kernel/patterns.js";
+import service from '../services/userServices'
+import { onError, onSuccess } from "@/kernel/alerts";
 export default {
   name: 'ModalSaveUser',
   props: {
     visible: Boolean,
     required: true
   },
-  data(){
+  data() {
     return {
       show: false,
       userTypes: [
-        {name: 'Administrador', code: 'admin'},
-        {name: 'Usuario', code: 'normal'}
+        { name: 'Administrador', code: 'Administrators' },
+        { name: 'Usuario', code: 'NormalUsers' }
       ],
       selectedUser: null
     }
   },
-  setup(){
+  setup() {
     const person = reactive({
       name: '',
       surname: '',
@@ -266,7 +259,7 @@ export default {
       user_type: {
         required: helpers.withMessage('El tipo de usuario es requerido', required),
       },
-      username : {
+      username: {
         required: helpers.withMessage('El nombre de usuario es requerido', required),
         onlyLetters: helpers.withMessage('El nombre de usuario solo puede contener letras y numeros', (value) => nameRegex.test(value)),
         minLength: helpers.withMessage("El nombre debe tener al menos 3 caracteres", minLength(3)),
@@ -275,15 +268,43 @@ export default {
 
     }
     const v$ = useVuelidate(rules, person)
-    return {person, v$}
+    return { person, v$ }
   },
   methods: {
-    closeModal(){
+    closeModal() {
       this.$emit('update:visible', false);
     },
-    saveUser(){
-      console.log('Guardando usuario')
-      console.log(this.person)
+    async saveUser() {
+      if (!this.v$.$invalid) {
+        this.isLoading = true
+
+        try {
+          const { status } = await service.save_user(this.prepareObject());
+          if (status === "success") {
+            onSuccess("¡Éxito!", "¡Usuario guardado correctamente!");
+          } else {
+            console.log(error);
+            console.log(this.person.user_type);
+          }
+        } catch (error) {
+
+          return error
+        }
+      } else {
+        onError("¡Error!", "¡Debes completar los campos correctamente!");
+      }
+    },
+    prepareObject() {
+      return {
+        username: this.person.username,
+        name: this.person.name,
+        last_name: this.person.lastname,
+        surname: this.person.surname,
+        phone: this.person.phone,
+        email: this.person.email,
+        password: this.person.password,
+        user_type: this.person.user_type
+      }
     }
   }
 }
@@ -291,6 +312,7 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/styles/colors';
+
 .button-options {
   background: $primary-color;
   color: white;
---
+++ front-contact-keeper/src/modules/users/services/userServices.js
@@ -0,0 +1,28 @@
+import api from "@/config/http-client.gateway";
+
+const get_users = async () => {
+  try {
+    const response = await api.doGet("/users");
+    return response.data;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const save_user = async (person) => {
+  try {
+    const response = await api.doPost("/users", person,{
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    });
+    return response.data
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export default {
+  get_users,
+  save_user,
+};
